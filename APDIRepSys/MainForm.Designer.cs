57d1141f464f92e7e9c1da3eec518bc39c38fe70
chuseokhanni <chuseokhanni@WILSONSYKES>
2025-01-28 03:22:54 +0800

    Add project files.

---
+++ APDIRepSys/MainForm.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace APDIRepSys
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.PaleTurquoise;
+            panel1.Dock = DockStyle.Right;
+            panel1.Location = new Point(367, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(673, 573);
+            panel1.TabIndex = 1;
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1040, 573);
+            Controls.Add(panel1);
+            IsMdiContainer = true;
+            Name = "MainForm";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Main Form";
+            WindowState = FormWindowState.Maximized;
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+    }
+}
